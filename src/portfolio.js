8310fbbf6f842e60442275ce94b070f0bfc5dbee
jigyansha rout <routjig09@gmail.com>
2025-06-11 22:51:23 +0530

    Initial commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,28 +20,25 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "JigyanshaRout",
+  title: "Hello all, I'm Jigyansha Rout",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Java Developer 🚀 having an experience of building Web and design applications ,software with JavaScript / Reactjs / Nodejs / Next.js and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1ETTaqkpQnYK5TkZ1qE0ekpXocScOymbe/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
+  github: "https://github.com/routjig09",
+  linkedin: " https://www.linkedin.com/in/jigyansha-rout-0b1a2b1b4/",
+  gmail: "routjig09@gmail.com",
+  medium: "https://medium.com/@routjig09",
+  instagram: "https://www.instagram.com/jigyansha_rout/",
+  x:"https://x.com/JigyanshaRout",
   display: true // Set true to display this section, defaults to false
 };
 
@@ -52,11 +49,11 @@ const skillsSection = {
   subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
-      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+      "⚡ I build full-stack web applications using Java, Spring Boot, and React.js with clean, scalable backend logic."
     ),
-    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji("⚡ I design sleek, responsive frontends that blend creativity with usability for a seamless user experience."),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Passionate about turning ideas into powerful digital solutions that solve real-world problems."
     )
   ],
 
@@ -73,10 +70,6 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-css3-alt"
     },
     {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
       skillName: "JavaScript",
       fontAwesomeClassname: "fab fa-js"
     },
@@ -87,10 +80,6 @@ https://fontawesome.com/icons?d=gallery */
     {
       skillName: "nodejs",
       fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
     },
     {
       skillName: "npm",
@@ -105,16 +94,15 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-aws"
     },
     {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
+      skillName: "Java",
+      fontAwesomeClassname: "fab fa-java"
     },
     {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
+      skillName: "SpringBoot",
     },
     {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
+      skillName: "MySQL",
+      fontAwesomeClassname: "fab fa-MySql"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -126,23 +114,22 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "GITA Autonomous college,Bhubaneswar",
+      logo: require("./assets/images/1717479631phpWNzymI.jpeg"),
+      subHeader: "Bachelor of Technology in Computer Science and Engineering",
+      duration: "Auguest 2021 - May 2025",
+      desc: "Participated in Smart India Hackathon 2023.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Pursuing B.Tech in Computer Science with a strong foundation in Data Structures, Algorithms, OOPs, DBMS, OS, and Computer Networks. Built academic projects in Java, web development, and AI-based systems. Actively engaged in coding contests, tech fests, and team-based projects, enhancing my problem-solving and collaboration skills"
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "SSVM Tulasipur,Cuttack",
+      logo: require("./assets/images/download.jpg"),
+      subHeader: "Higher Secondary Education in Science",
+      duration: "June 2019 - June 2021",
+      desc: " Completed Higher Secondary Education with a focus on Science, achieving a score of 72% in the board examinations",
+      descBullets: ["Completed 12th grade with a focus on Science PCMB, building a strong analytical foundation and discipline. Participated in academic and extracurricular activities that developed teamwork and leadership skills."]
     }
   ]
 };
@@ -158,11 +145,11 @@ const techStack = {
     },
     {
       Stack: "Backend",
-      progressPercentage: "70%"
+      progressPercentage: "60%"
     },
     {
       Stack: "Programming",
-      progressPercentage: "60%"
+      progressPercentage: "80%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -174,29 +161,29 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
+      role: "Java Developer Intern",
+      company: "AICTE Internship",
+      companylogo: require("./assets/images/unnamed.jpg"),
+      date: "Aug 2024 – oct 2024",
+      desc: " completed a certified Java Development internship under the AICTE-approved Plasmid Academy program, where I built real-world Java applications using core and advanced concepts. During the internship, I developed a Bank Management System with MySQL integration and multi-role login functionality. This hands-on experience deepened my backend skills and strengthened my understanding of database-driven Java projects.",
+      // descBullets: [
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      // ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Software Enginnering",
+      company: "Forage Virtual Experience Program",
+      companylogo: require("./assets/images/images.jpg"),
+      date: "june 2025 –  present",
+      desc: "I developed a Finance Track App to help users manage income, expenses, and savings efficiently through a clean, user-friendly interface. The app features transaction tracking, monthly summaries, and visual analytics to promote smarter financial decisions. Built using Java and MySQL, it demonstrates my backend development skills and real-world problem-solving ability in personal finance management."
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Advance Software Engineering Intern",
+      company: "Walmart GlobalTech",
+      companylogo: require("./assets/images/channels4_profile.jpg"),
+      date: "May 2025 – May 2025",
+      desc: "Completed the Walmart Global Tech Virtual Experience Program on Forage, where I tackled real-world software engineering tasks including debugging, feature optimization, and code review. Gained hands-on experience with agile methodologies, version control, and scalable backend systems. This internship enhanced my technical problem-solving and exposed me to Walmart’s global engineering practices."
     }
   ]
 };
@@ -212,13 +199,12 @@ const openSource = {
 // Some big projects you have worked on
 
 const bigProjects = {
-  title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  title: "Projects",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/vector-abstract-line-art-book-600nw-2472789681.webp"),
+      projectName: "Finance Track App",
+      projectDesc: "A Finance Track App to help users manage income, expenses, and savings efficiently through a clean, user-friendly interface.",
       footerLink: [
         {
           name: "Visit Website",
@@ -228,9 +214,9 @@ const bigProjects = {
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/original-ad8c694bb61035244572693e8ff928f6.webp"),
+      projectName: "Virtual Library ",
+      projectDesc: "Co-developed a Virtual Library app to buy and sell new or second-hand books affordably, featuring user login etc..",
       footerLink: [
         {
           name: "Visit Website",
@@ -252,50 +238,50 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: " ICT Academy & Ministry of Education",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "Completed a skill development program organized by ICT Academy and the Ministry of Education, focusing on industry-based training to enhance technical and employability skills..",
+      image: require("./assets/images/images.png"),
+      // imageAlt: "Google Code-In Logo",
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          url: "https://drive.google.com/file/d/1peECi6Kswk0m4wiRqO4jTdX3UVko3KyA/view?usp=sharing"
         },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        }
+        // {
+        //   name: "Award Letter",
+        //   url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+        // },
+        // {
+        //   name: "Google Code-in Blog",
+        //   url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+        // }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Developer Student Clubs",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
+        "Completed a hands-on technical training program by Devtown of developer student club, focused on practical skills and real-world project implementation.",
+      image: require("./assets/images/54115493.jpg"),
       imageAlt: "Google Assistant Action Logo",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Certification",
+          url: "https://drive.google.com/file/d/13zNj2q3ApbaNKe15geGNwGd7HY05lgg_/view?usp=sharing"
         }
       ]
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
+      title: "Microsoft Learn Student Ambassador",
+      subtitle: "Completed a Microsoft Learn training program through Devtown, gaining hands-on experience in Microsoft technologies and practical development skills.",
+      image: require("./assets/images/microsoft.jpg"),
+      // imageAlt: "PWA Logo",
       footerLink: [
-        {name: "Certification", url: ""},
+        
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Certification",
+          url: "https://drive.google.com/file/d/13zv4FU0i58zZPRHHs-MSNO5pxNYmybv0/view?usp=sharing"
         }
       ]
     }
@@ -324,7 +310,7 @@ const blogSection = {
         "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Talks Sections
@@ -343,7 +329,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,7 +342,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -372,8 +358,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+91-7684972630",
+  email_address: "routjig@gmail.com"
 };
 
 // Twitter Section
